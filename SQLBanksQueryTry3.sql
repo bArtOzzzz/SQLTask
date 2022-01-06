20a68b7e7927c2f42f775d3d31a57db47e060bfa
bArtOzzzz <breakyourbrain55@gmail.com>
2022-01-06 19:15:53 +0300

    Update SQLBanksQueryTry3.sql

--- SQLBanksQueryTry3.sql
+++ SQLBanksQueryTry3.sql
@@ -231,247 +231,71 @@ union select 32, 32, 35
 union select 33, 33, 700
 union select 34, 34, 210.20
 
-
-
 --------------------------------------------------Query-1----------------------------------------------
 ------Displays a list of banks that have a branch in city X
---GO
---SELECT BankInfo.BankId, BankName, City 
---FROM BankInfo RIGHT JOIN BankBranches ON BankInfo.BankId = BankBranches.BankId 
---WHERE City = 'Brest' 
---ORDER BY BankName
+GO
+SELECT BankInfo.BankId, City.City, BankInfo.BankName 
+FROM BankInfo INNER JOIN BankBranches ON BankInfo.BankId = BankBranches.BankId 
+			  INNER JOIN City ON City.CityId = BankBranches.CityId
+WHERE BankBranches.CityId = 2
 
 --------------------------------------------------Query-2----------------------------------------------
 ------Displays a list of cards indicating the name of the owner, balance and the name of the bank
---GO
---SELECT CardInfo.CardId, UserName, CardBalance, BankName
---FROM CardInfo LEFT JOIN UserInfo ON CardInfo.CardId = UserInfo.CardId INNER JOIN BankInfo ON CardInfo.BankId = BankInfo.BankId
-----ORDER BY UserCardName
+GO
+SELECT CardInfo.CardId, BankInfo.BankName, UserInfo.FirstName, UserInfo.LastName, CardInfo.CardBalance 
+FROM CardInfo INNER JOIN Account ON Account.AccountId = CardInfo.AccountId 
+			  INNER JOIN UserInfo ON UserInfo.UserId = Account.UserId
+			  INNER JOIN BankInfo ON Account.BankId = BankInfo.BankId
 
 --------------------------------------------------Query-3----------------------------------------------
 ------Displays a list of bank accounts where balance doesn't match the sum of the card balance and shows the difference
---GO
---SELECT AccountName, AccountBalance, CardBalance, (AccountBalance - CardBalance) DifferenceVal
---FROM Account JOIN CardInfo ON CardInfo.CardId = Account.CardId
---WHERE AccountBalance != CardBalance
-
-------------------------------------------------------------END----------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---USE master
---if exists(select * from sys.tables where name='BankInfo') drop table BankInfo
---if exists(select * from sys.tables where name='BankBranches' ) drop table BankBranches
---if exists(select * from sys.tables where name='UserCardInfo' ) drop table UserCardInfo
---if exists(select * from sys.tables where name='UserAccountInfo' ) drop table UserAccountInfo
-
---if exists(select * from sys.procedures where name='TransactionProcedure' ) drop procedure TransactionProcedure
---if exists(select * from sys.procedures where name='AddTenDollars' ) drop procedure AddTenDollars
-
---if exists(select * from sys.triggers where name='UserCardsTrigger' ) drop trigger UserCardsTrigger
---if exists(select * from sys.triggers where name='UserAccountsTrigger' ) drop trigger UserAccountsTrigger
---GO
-----------------------------------------------------TABLES STRUCTURE-----------------------------------------
---CREATE TABLE BankInfo
---(
---	BankId int,
---	BankName varchar(55),
---	--BankTransitAmount decimal(19,2),
---)
---GO
---CREATE TABLE BankBranches
---(
---	BankId int,
---	City varchar(125),
---	--Adress varchar(55)
---)
---GO
---CREATE TABLE UserInfo
---(
---	CardId int,
---	UserName varchar(125),
---	UserAge int,
---	City varchar(125)
---)
---GO
---CREATE TABLE CardInfo
---(
---	CardId int,
---	BankId int,
---	--UserCardName varchar(125),
---	CardBalance decimal(6,2),
---	--TotalCardsBalance decimal(6,2)
---)
---CREATE TABLE AccountInfo
---(
---	BankId int,
---	AccountName varchar(125),
---	SocialStatus varchar(125),
---	AccountBalance decimal(6,2),
---	--TotalAccountBalance decimal(6,2)
---)
---INSERT BankInfo
---	  select 1, 'Alpha-Bank', 1000500
---union select 2, 'Belinvestbank', 190000.23
---union select 3, 'Technobank', 785000.45
---union select 4, 'BelWeb', 123123123.43
---union select 5, 'MTBank', 4587555
---union select 6, 'Belarusbank', 800800
-
---GO
---INSERT BankBranches
---	  select 1, 'Gomel'
---union select 2, 'Gomel'
---union select 3, 'Gomel'
---union select 4, 'Gomel'
---union select 5, 'Gomel'
---union select 6, 'Gomel'
-
---union select 1, 'Brest'
---union select 2, 'Brest'
---union select 3, 'Brest'
-
---union select 4, 'Grodno'
---union select 5, 'Grodno'
---union select 1, 'Grodno'
---union select 3, 'Grodno'
-
---union select 1, 'Mogilev'
---union select 2, 'Mogilev'
---union select 3, 'Mogilev'
---union select 4, 'Mogilev'
---union select 5, 'Mogilev'
---union select 6, 'Mogilev'
-
---union select 5, 'Vitebsk'
---union select 6, 'Vitebsk'
---GO
---INSERT UserCardInfo
---	  select 1, 1, 'Anastasia', 100.25, null
---union select 2, 2, 'Anastasia', 125, null
-
---union select 3, 2, 'Lev', 10.20, null
-
---union select 4, 3, 'Anna', 65.90, null
---union select 5, 3, 'Anna', 0.50, null
---union select 6, 4, 'Anna', 230.90, null
---union select 7, 5, 'Anna', 115.45, null
-
---union select 8, 6, 'Maria', 230.95, null
---union select 9, 5, 'Maria', 55.55, null
---union select 10, 5, 'Maria', 1100,  null
-
---union select 11, 4, 'Alexander', 2000.15, null
---union select 12, 6, 'Alexander', 1340.40, null
---union select 13, 2, 'Alexander', 2022, null
---GO
---INSERT UserAccountInfo
---	  select 1, 'Anastasiya@gmail.com', 'Worker', 100.25, null
---union select 2, 'Anastasiya@gmail.com', 'Worker', 150, null
-
---union select 3, 'Lev@gmail.com', 'Pensioner', 100.75, null
-
---union select 4, 'Anna@gmail.com', 'Disable', 65.90, null
---union select 5, 'Anna@gmail.com', 'Disable', 0.50, null
---union select 6, 'Anna@gmail.com', 'Disable', 730.90, null
---union select 7, 'Anna@gmail.com', 'Disable', 190, null
-
---union select 8, 'Maria@gmail.com', 'Student', 275.45, null
---union select 9, 'Maria@gmail.com', 'Student', 55.55, null
---union select 10, 'Maria@gmail.com', 'Student', 1500, null
-
---union select 11, 'Alexander@gmail.com', 'Worker', 2022.15, null
---union select 12, 'Alexander@gmail.com', 'Worker', 1765.35, null
---union select 13, 'Alexander@gmail.com', 'Worker', 2022, null
-
-------------------------------------------------Query-1----------------------------------------------
-----Displays a list of banks that have a branch in city X
---GO
---SELECT BankName, City 
---FROM BankInfo RIGHT JOIN BankBranches ON BankInfo.BankId = BankBranches.BankId 
---WHERE City = 'Brest' 
---ORDER BY BankName
-
-------------------------------------------------Query-2----------------------------------------------
-----Displays a list of cards indicating the name of the owner, balance and the name of the bank
---GO
---SELECT CardId, BankInfo.BankId, UserCardName, BankName, CardBalance 
---FROM UserCardInfo LEFT JOIN BankInfo ON UserCardInfo.BankId = BankInfo.BankId
---ORDER BY UserCardName
-
-------------------------------------------------Query-3----------------------------------------------
-----Displays a list of bank accounts where balance doesn't match the sum of the card balance and shows the difference
---GO
---SELECT UserAccountName, CardBalance, AccountBalance, (AccountBalance - CardBalance) DifferenceVal 
---FROM UserAccountInfo INNER JOIN UserCardInfo ON CardId = AccountId 
---WHERE AccountBalance != CardBalance
+GO
+SELECT Account.AccountId, Account.AccountName, Account.AccountBalance, CardInfo.CardBalance, (Account.AccountBalance - CardInfo.CardBalance) AS DifferenceVal
+FROM Account INNER JOIN CardInfo ON CardInfo.AccountId = Account.AccountId
+WHERE Account.AccountBalance != CardInfo.CardBalance
 
 ------------------------------------------------Query-4----------------------------------------------
 ----Display the number of the bank cards for each social status: Group by
---GO
---SELECT SocialStatus, COUNT(SocialStatus) CountCards 
---FROM UserAccountInfo 
---WHERE SocialStatus IS NOT NULL 
---GROUP BY SocialStatus 
---ORDER BY CountCards DESC
+GO
+SELECT UserStatus.SocialStatus, COUNT(UserStatus.SocialStatus) AS CountCards
+FROM UserStatus INNER JOIN UserInfo ON UserInfo.SocialStatusId = UserStatus.SocialStatusId
+GROUP BY UserStatus.SocialStatus
+ORDER BY CountCards
 
 ----Display the number of the bank cards for each social status: Subquery
---GO
---SELECT SocialStatus, COUNT(SocialStatus) CountCards
---FROM
---(
---	SELECT SocialStatus, COUNT(SocialStatus) CountCards
---	FROM UserAccountInfo INNER JOIN UserCardInfo ON AccountId = CardId
---	GROUP BY SocialStatus, AccountId
---)t1
---GROUP BY SocialStatus 
---ORDER BY CountCards DESC
+GO
+SELECT SocialStatus, COUNT(SocialStatus) AS CountCards 
+FROM 
+(
+	SELECT UserStatus.SocialStatus, UserStatus.SocialStatus AS CountCards
+	FROM UserStatus INNER JOIN UserInfo ON UserInfo.SocialStatusId = UserStatus.SocialStatusId
+)t1
+GROUP BY SocialStatus
+ORDER BY CountCards DESC
 
 ------------------------------------------------Query-5----------------------------------------------
 ----Add 10$ for each bank account for a special social status
---GO
---CREATE PROCEDURE AddTenDollars 
---(
---	@SocialStatus varchar(125), @AddSomeMoney decimal(6,2)
---)
---AS
---BEGIN 
---	SELECT UserAccountName, AccountBalance, @AddSomeMoney PlusMoney,(AccountBalance + @AddSomeMoney) AfterAddMoney 
---	FROM UserAccountInfo
---	WHERE SocialStatus = @SocialStatus
---END
---GO
---EXEC AddTenDollars 'Student', 30
+GO
+CREATE PROCEDURE AddTenDollars
+(
+	@SocialStatus varchar(125), @AddSomeMoney decimal(6,2)
+)
+AS
+BEGIN
+	SELECT Account.AccountName, Account.AccountBalance, @AddSomeMoney AS AddMoney, (Account.AccountBalance + @AddSomeMoney) AS AfterAddMoney, UserStatus.SocialStatus
+	FROM Account INNER JOIN UserInfo ON UserInfo.UserId = Account.UserId
+				 INNER JOIN UserStatus ON UserStatus.SocialStatusId = UserInfo.SocialStatusId
+	WHERE UserInfo.SocialStatusId = 1
+END
+GO
+EXEC AddTenDollars 1, 30
 
 ------------------------------------------------Query-6----------------------------------------------
 ----Displays a list of available funds for each client to transfer funds to any card
 ----Available transfer amount for each account on an accrual basis
---GO
---SELECT UserAccountName, AccountBalance, CardBalance, sum(AccountBalance - CardBalance) OVER(PARTITION BY UserAccountName ORDER BY CardBalance DESC) AvailableTransferAmount
---FROM UserAccountInfo INNER JOIN UserCardInfo ON CardId = AccountId
+GO
+SELECT Account.AccountName, Account.AccountBalance, CardInfo.CardBalance, SUM(Account.AccountBalance - CardInfo.CardBalance) OVER(PARTITION BY Account.AccountName ORDER BY CardInfo.CardBalance DESC) AS AvailableTransferAmount
+FROM Account INNER JOIN CardInfo ON CardInfo.AccountId = Account.AccountId
 
 ------------------------------------------------Query-7----------------------------------------------
 ----Procedure that safely transfer money from an account to a card of this account using transaction
