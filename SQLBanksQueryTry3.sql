eece1bdb05d6073b0bf506821fd7eb2dbc04032c
bArtOzzzz <breakyourbrain55@gmail.com>
2022-01-10 15:07:46 +0300

    Update SQLBanksQueryTry3.sql

--- SQLBanksQueryTry3.sql
+++ SQLBanksQueryTry3.sql
@@ -142,94 +142,80 @@ union select 6, 5, 'Any adress'
 GO
 INSERT Account
 	  select  1, 1,  1, 'SomeEmail_1@gmail.com', 100.25
-union select  2, 1,  1, 'SomeEmail_1@gmail.com', 150
-
-union select  3, 2,  2, 'SomeEmail_2@gmail.com', 1500
-union select  4, 5,  2, 'SomeEmail_2@gmail.com', 10
-union select  5, 1,  2, 'SomeEmail_2@gmail.com', 0.20
-
-union select  6, 3,  3, 'SomeEmail_3@gmail.com', 120.30
-union select  7, 3,  3, 'SomeEmail_3@gmail.com', 560.35
-
-union select  8, 2,  4, 'SomeEmail_4@gmail.com', 755.35
-
-union select  9, 1,  5, 'SomeEmail_5@gmail.com', 20.40
-
-union select 10, 1,  6, 'SomeEmail_6@gmail.com', 355
-union select 11, 4,  6, 'SomeEmail_6@gmail.com', 450
-
-union select 12, 1,  7, 'SomeEmail_7@gmail.com', 115.15
-
-union select 13, 1,  8, 'SomeEmail_8@gmail.com', 855.15
-union select 14, 3,  8, 'SomeEmail_8@gmail.com', 345.75
-union select 15, 1,  8, 'SomeEmail_8@gmail.com', 340
-
-union select 16, 2,  9, 'SomeEmail_9@gmail.com', 230.30
-
-union select 17, 3, 10, 'SomeEmail_10@gmail.com', 150
-
-union select 18, 2, 11, 'SomeEmail_11@gmail.com', 670
-union select 19, 1, 11, 'SomeEmail_11@gmail.com', 345
-
-union select 20, 1, 12, 'SomeEmail_12@gmail.com', 125
-union select 21, 1, 12, 'SomeEmail_12@gmail.com', 455
-
-union select 22, 4, 13, 'SomeEmail_13@gmail.com', 345
-union select 23, 4, 13, 'SomeEmail_13@gmail.com', 785
-
-union select 24, 1, 14, 'SomeEmail_14@gmail.com', 560
-union select 25, 5, 14, 'SomeEmail_14@gmail.com', 370
-union select 26, 5, 14, 'SomeEmail_14@gmail.com', 810
-
-union select 27, 1, 15, 'SomeEmail_15@gmail.com', 0.80
-
-union select 28, 2, 16, 'SomeEmail_16@gmail.com', 20
-
-union select 29, 3, 17, 'SomeEmail_17@gmail.com', 30
-
-union select 30, 1, 18, 'SomeEmail_18@gmail.com', 65
-union select 31, 3, 18, 'SomeEmail_18@gmail.com', 40
-
-union select 32, 1, 19, 'SomeEmail_19@gmail.com', 35
-union select 33, 2, 19, 'SomeEmail_19@gmail.com', 700
-
-union select 34, 1, 20, 'SomeEmail_20@gmail.com', 365
+union select  2, 2,  2, 'SomeEmail_2@gmail.com', 1500
+union select  3, 3,  3, 'SomeEmail_3@gmail.com', 120.30
+union select  4, 2,  4, 'SomeEmail_4@gmail.com', 755.35
+union select  5, 1,  5, 'SomeEmail_5@gmail.com', 730
+union select  6, 1,  6, 'SomeEmail_6@gmail.com', 390
+union select  7, 1,  7, 'SomeEmail_7@gmail.com', 115.15
+union select  8, 1,  8, 'SomeEmail_8@gmail.com', 855.15
+union select  9, 2,  9, 'SomeEmail_9@gmail.com', 230.30
+union select 10, 3, 10, 'SomeEmail_10@gmail.com', 150
+union select 11, 2, 11, 'SomeEmail_11@gmail.com', 670
+union select 12, 1, 12, 'SomeEmail_12@gmail.com', 125
+union select 13, 4, 13, 'SomeEmail_13@gmail.com', 345
+union select 14, 1, 14, 'SomeEmail_14@gmail.com', 560
+union select 15, 1, 15, 'SomeEmail_15@gmail.com', 371.35
+union select 16, 2, 16, 'SomeEmail_16@gmail.com', 20
+union select 17, 3, 17, 'SomeEmail_17@gmail.com', 100
+union select 18, 1, 18, 'SomeEmail_18@gmail.com', 65
+union select 19, 1, 19, 'SomeEmail_19@gmail.com', 35
+union select 20, 1, 20, 'SomeEmail_20@gmail.com', 1000
 GO
 INSERT CardInfo
 	  select 1, 1, 90
-union select 2, 2, 100
-union select 3, 3, 1500
-union select 4, 4, 0
-union select 5, 5, 0.20
-union select 6, 6, 110
-union select 7, 7, 320
-union select 8, 8, 730
-union select 9, 9, 20.40
-union select 10, 10, 355
-union select 11, 11, 350
-union select 12, 12, 0
-union select 13, 13, 345.90
-union select 14, 14, 120
-union select 15, 15, 210.35
-union select 16, 16, 130.50
-union select 17, 17, 150
-union select 18, 18, 670
-union select 19, 19, 345
-union select 20, 20, 120
-union select 21, 21, 120.45
-union select 22, 22, 155.15
-union select 23, 23, 785
-union select 24, 24, 325.35
-union select 25, 25, 140.40
-union select 26, 26, 370.55
-union select 27, 27, 0.80
-union select 28, 28, 20
-union select 29, 29, 15.10
-union select 30, 30, 40.75
-union select 31, 31, 25.60
-union select 32, 32, 35
-union select 33, 33, 700
-union select 34, 34, 210.20
+union select 2, 1, 10
+
+union select 3, 2, 1500
+union select 4, 2, 0
+
+union select 5, 3, 0.20
+
+union select 6, 4, 110
+union select 7, 4, 320
+
+union select 8, 5, 730
+
+union select 9, 6, 20.40
+union select 10, 6, 355
+
+union select 11, 7, 115.15
+union select 12, 7, 0
+
+union select 13, 8, 345.90
+union select 14, 8, 120
+union select 15, 8, 210.35
+
+union select 16, 9, 230.30
+
+union select 17, 10, 150
+
+union select 18, 11, 180
+union select 19, 11, 345
+
+union select 20, 12, 120
+
+union select 21, 13, 120.45
+union select 22, 13, 155.15
+
+union select 23, 14, 20
+union select 24, 14, 325.35
+union select 25, 14, 140.40
+
+union select 26, 15, 370.55
+union select 27, 15, 0.80
+
+union select 28, 16, 20
+
+union select 29, 17, 15.10
+union select 30, 17, 40.75
+
+union select 31, 18, 25.60
+
+union select 32, 19, 35
+
+union select 33, 20, 700
+union select 34, 20, 210.20
 
 --------------------------------------------------Query-1----------------------------------------------
 ------Displays a list of banks that have a branch in city X
@@ -250,9 +236,18 @@ FROM CardInfo INNER JOIN Account ON Account.AccountId = CardInfo.AccountId
 --------------------------------------------------Query-3----------------------------------------------
 ------Displays a list of bank accounts where balance doesn't match the sum of the card balance and shows the difference
 GO
-SELECT Account.AccountName, (Account.AccountBalance - CardInfo.CardBalance) AS DifferenceVal
+SELECT SUM(CardInfo.CardBalance) AS SumCardBalance
 FROM Account INNER JOIN CardInfo ON CardInfo.AccountId = Account.AccountId
-WHERE Account.AccountBalance != CardInfo.CardBalance
+GROUP BY CardInfo.AccountId
+
+SELECT CardInfo.CardBalance - Account.AccountBalance AS SumCardBalance
+FROM Account INNER JOIN CardInfo ON CardInfo.AccountId = Account.AccountId
+GROUP BY CardInfo.AccountId
+
+SELECT SUM(Account.AccountBalance)
+FROM Account 
+GROUP BY Account.AccountId
+
 
 ------------------------------------------------Query-4----------------------------------------------
 ----Display the number of the bank cards for each social status: Group by
