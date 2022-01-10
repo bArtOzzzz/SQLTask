abbd3e80ea6fdceb9772243ac60e6ab4e5dbff78
bArtOzzzz <breakyourbrain55@gmail.com>
2022-01-10 12:39:28 +0300

    Update SQLBanksQueryTry3.sql

--- SQLBanksQueryTry3.sql
+++ SQLBanksQueryTry3.sql
@@ -11,7 +11,7 @@ GO
 CREATE TABLE City
 (
 	CityId int not null,
-	City varchar(125),
+	CityName varchar(125),
 
 	PRIMARY KEY (CityId)
 )
@@ -92,26 +92,26 @@ union select 3, 'Pensioner'
 union select 4, 'Disable'
 GO
 INSERT UserInfo
-	  select 1,  1, 'Тихомирова',	'Елизавета', 1,	18
-union select 2,  1, 'Смирнов',		'Максим',	 2,	26
-union select 3,  2, 'Кудряшов',		'Павел',	 2,	23
-union select 4,  4, 'Куприянов',	'Владимир',	 1,	20
-union select 5,  5, 'Жданов',		'Мирон',	 1,	21
-union select 6,  2, 'Шевелев',		'Илья',		 2,	37
-union select 7,  3, 'Краснов',		'Григорий',  3,	68
-union select 8,  1, 'Соколова',		'Екатерина', 2,	30
-union select 9,  1, 'Степанова',	'Фатима',	 3,	66
-union select 10, 2, 'Соколов',		'Владимир',	 2,	42
-union select 11, 3, 'Иванова',		'Вера',		 4,	51
-union select 12, 5, 'Филиппова',	'Надежда',	 3,	72
-union select 13, 1, 'Беспалова',	'Екатерина', 2,	22
-union select 14, 1, 'Галкин',		'Савва',	 2,	25
-union select 15, 2, 'Попов',		'Александр', 3,	62
-union select 16, 3, 'Новиков',		'Ибрагим',	 2,	33
-union select 17, 4, 'Смирнов',		'Александр', 3,	71
-union select 18, 5, 'Ткачев',		'Виктор',	 4,	44
-union select 19, 3, 'Захаров',		'Адам',		 4,	39
-union select 20, 1, 'Прокофьев',	'Билал',	 2,	28
+	  select 1,  1, 'Tikhomirova',	'Elizabeth', 1,	18
+union select 2,  1, 'Smirnov',		'Maksim',	 2,	26
+union select 3,  2, 'Kudryashov',	'Pavel',	 2,	23
+union select 4,  4, 'Kupriyanov',	'Vladimir',	 1,	20
+union select 5,  5, 'Zhdanov',		'Miron',	 1,	21
+union select 6,  2, 'Shevelev',		'Ilya',		 2,	37
+union select 7,  3, 'Krasnov',		'Gregory',   3,	68
+union select 8,  1, 'Sokolova',		'Ekaterina', 2,	30
+union select 9,  1, 'Stepanova',	'Fatima',	 3,	66
+union select 10, 2, 'Sokolov',		'Vladimir',	 2,	42
+union select 11, 3, 'Ivanova',		'Faith',	 4,	51
+union select 12, 5, 'Filippova',	'Hope',		 3,	72
+union select 13, 1, 'Bespalova',	'Ekaterina', 2,	22
+union select 14, 1, 'Galkin',		'Savva',	 2,	25
+union select 15, 2, 'Popov',		'Alexander', 3,	62
+union select 16, 3, 'Novikov',		'Ibrahim',	 2,	33
+union select 17, 4, 'Smirnov',		'Alexander', 3,	71
+union select 18, 5, 'Tkachev',		'Victor',	 4,	44
+union select 19, 3, 'Zakharov',		'Adam',		 4,	39
+union select 20, 1, 'Prokofiev',	'Bilal',	 2,	28
 GO
 INSERT BankBranches
 	  select 1, 1, 'Any adress'
@@ -141,59 +141,59 @@ union select 5, 5, 'Any adress'
 union select 6, 5, 'Any adress'
 GO
 INSERT Account
-	  select  1, 1,  1, 'SomeEmail@gmail.com', 100.25
-union select  2, 1,  1, 'SomeEmail@gmail.com', 150
+	  select  1, 1,  1, 'SomeEmail_1@gmail.com', 100.25
+union select  2, 1,  1, 'SomeEmail_1@gmail.com', 150
 
-union select  3, 2,  2, 'SomeEmail@gmail.com', 1500
-union select  4, 5,  2, 'SomeEmail@gmail.com', 10
-union select  5, 1,  2, 'SomeEmail@gmail.com', 0.20
+union select  3, 2,  2, 'SomeEmail_2@gmail.com', 1500
+union select  4, 5,  2, 'SomeEmail_2@gmail.com', 10
+union select  5, 1,  2, 'SomeEmail_2@gmail.com', 0.20
 
-union select  6, 3,  3, 'SomeEmail@gmail.com', 120.30
-union select  7, 3,  3, 'SomeEmail@gmail.com', 560.35
+union select  6, 3,  3, 'SomeEmail_3@gmail.com', 120.30
+union select  7, 3,  3, 'SomeEmail_3@gmail.com', 560.35
 
-union select  8, 2,  4, 'SomeEmail@gmail.com', 755.35
+union select  8, 2,  4, 'SomeEmail_4@gmail.com', 755.35
 
-union select  9, 1,  5, 'SomeEmail@gmail.com', 20.40
+union select  9, 1,  5, 'SomeEmail_5@gmail.com', 20.40
 
-union select 10, 1,  6, 'SomeEmail@gmail.com', 355
-union select 11, 4,  6, 'SomeEmail@gmail.com', 450
+union select 10, 1,  6, 'SomeEmail_6@gmail.com', 355
+union select 11, 4,  6, 'SomeEmail_6@gmail.com', 450
 
-union select 12, 1,  7, 'SomeEmail@gmail.com', 115.15
+union select 12, 1,  7, 'SomeEmail_7@gmail.com', 115.15
 
-union select 13, 1,  8, 'SomeEmail@gmail.com', 855.15
-union select 14, 3,  8, 'SomeEmail@gmail.com', 345.75
-union select 15, 1,  8, 'SomeEmail@gmail.com', 340
+union select 13, 1,  8, 'SomeEmail_8@gmail.com', 855.15
+union select 14, 3,  8, 'SomeEmail_8@gmail.com', 345.75
+union select 15, 1,  8, 'SomeEmail_8@gmail.com', 340
 
-union select 16, 2,  9, 'SomeEmail@gmail.com', 230.30
+union select 16, 2,  9, 'SomeEmail_9@gmail.com', 230.30
 
-union select 17, 3, 10, 'SomeEmail@gmail.com', 150
+union select 17, 3, 10, 'SomeEmail_10@gmail.com', 150
 
-union select 18, 2, 11, 'SomeEmail@gmail.com', 670
-union select 19, 1, 11, 'SomeEmail@gmail.com', 345
+union select 18, 2, 11, 'SomeEmail_11@gmail.com', 670
+union select 19, 1, 11, 'SomeEmail_11@gmail.com', 345
 
-union select 20, 1, 12, 'SomeEmail@gmail.com', 125
-union select 21, 1, 12, 'SomeEmail@gmail.com', 455
+union select 20, 1, 12, 'SomeEmail_12@gmail.com', 125
+union select 21, 1, 12, 'SomeEmail_12@gmail.com', 455
 
-union select 22, 4, 13, 'SomeEmail@gmail.com', 345
-union select 23, 4, 13, 'SomeEmail@gmail.com', 785
+union select 22, 4, 13, 'SomeEmail_13@gmail.com', 345
+union select 23, 4, 13, 'SomeEmail_13@gmail.com', 785
 
-union select 24, 1, 14, 'SomeEmail@gmail.com', 560
-union select 25, 5, 14, 'SomeEmail@gmail.com', 370
-union select 26, 5, 14, 'SomeEmail@gmail.com', 810
+union select 24, 1, 14, 'SomeEmail_14@gmail.com', 560
+union select 25, 5, 14, 'SomeEmail_14@gmail.com', 370
+union select 26, 5, 14, 'SomeEmail_14@gmail.com', 810
 
-union select 27, 1, 15, 'SomeEmail@gmail.com', 0.80
+union select 27, 1, 15, 'SomeEmail_15@gmail.com', 0.80
 
-union select 28, 2, 16, 'SomeEmail@gmail.com', 20
+union select 28, 2, 16, 'SomeEmail_16@gmail.com', 20
 
-union select 29, 3, 17, 'SomeEmail@gmail.com', 30
+union select 29, 3, 17, 'SomeEmail_17@gmail.com', 30
 
-union select 30, 1, 18, 'SomeEmail@gmail.com', 65
-union select 31, 3, 18, 'SomeEmail@gmail.com', 40
+union select 30, 1, 18, 'SomeEmail_18@gmail.com', 65
+union select 31, 3, 18, 'SomeEmail_18@gmail.com', 40
 
-union select 32, 1, 19, 'SomeEmail@gmail.com', 35
-union select 33, 2, 19, 'SomeEmail@gmail.com', 700
+union select 32, 1, 19, 'SomeEmail_19@gmail.com', 35
+union select 33, 2, 19, 'SomeEmail_19@gmail.com', 700
 
-union select 34, 1, 20, 'SomeEmail@gmail.com', 365
+union select 34, 1, 20, 'SomeEmail_20@gmail.com', 365
 GO
 INSERT CardInfo
 	  select 1, 1, 90
@@ -234,15 +234,15 @@ union select 34, 34, 210.20
 --------------------------------------------------Query-1----------------------------------------------
 ------Displays a list of banks that have a branch in city X
 GO
-SELECT BankInfo.BankId, City.City, BankInfo.BankName 
+SELECT BankInfo.BankName 
 FROM BankInfo INNER JOIN BankBranches ON BankInfo.BankId = BankBranches.BankId 
 			  INNER JOIN City ON City.CityId = BankBranches.CityId
-WHERE BankBranches.CityId = 2
+WHERE City.CityName = 'Brest'
 
 --------------------------------------------------Query-2----------------------------------------------
 ------Displays a list of cards indicating the name of the owner, balance and the name of the bank
 GO
-SELECT CardInfo.CardId, BankInfo.BankName, UserInfo.FirstName, UserInfo.LastName, CardInfo.CardBalance 
+SELECT BankInfo.BankName, UserInfo.FirstName, CardInfo.CardBalance 
 FROM CardInfo INNER JOIN Account ON Account.AccountId = CardInfo.AccountId 
 			  INNER JOIN UserInfo ON UserInfo.UserId = Account.UserId
 			  INNER JOIN BankInfo ON Account.BankId = BankInfo.BankId
@@ -250,7 +250,7 @@ FROM CardInfo INNER JOIN Account ON Account.AccountId = CardInfo.AccountId
 --------------------------------------------------Query-3----------------------------------------------
 ------Displays a list of bank accounts where balance doesn't match the sum of the card balance and shows the difference
 GO
-SELECT Account.AccountId, Account.AccountName, Account.AccountBalance, CardInfo.CardBalance, (Account.AccountBalance - CardInfo.CardBalance) AS DifferenceVal
+SELECT Account.AccountName, (Account.AccountBalance - CardInfo.CardBalance) AS DifferenceVal
 FROM Account INNER JOIN CardInfo ON CardInfo.AccountId = Account.AccountId
 WHERE Account.AccountBalance != CardInfo.CardBalance
 
@@ -264,14 +264,12 @@ ORDER BY CountCards
 
 ----Display the number of the bank cards for each social status: Subquery
 GO
-SELECT SocialStatus, COUNT(SocialStatus) AS CountCards 
-FROM 
-(
-	SELECT UserStatus.SocialStatus, UserStatus.SocialStatus AS CountCards
-	FROM UserStatus INNER JOIN UserInfo ON UserInfo.SocialStatusId = UserStatus.SocialStatusId
-)t1
-GROUP BY SocialStatus
-ORDER BY CountCards DESC
+SELECT UserStatus.SocialStatus, 
+	(SELECT COUNT(UserInfo.SocialStatusId) 
+	FROM UserInfo
+	WHERE UserStatus.SocialStatusId = UserInfo.SocialStatusId)  AS CountCards
+FROM UserStatus
+ORDER BY CountCards
 
 ------------------------------------------------Query-5----------------------------------------------
 ----Add 10$ for each bank account for a special social status
@@ -282,19 +280,21 @@ CREATE PROCEDURE AddTenDollars
 )
 AS
 BEGIN
-	SELECT Account.AccountName, Account.AccountBalance, @AddSomeMoney AS AddMoney, (Account.AccountBalance + @AddSomeMoney) AS AfterAddMoney, UserStatus.SocialStatus
+	SELECT Account.AccountName, @AddSomeMoney AS AddMoney, (Account.AccountBalance + @AddSomeMoney) AS AfterAddMoney
 	FROM Account INNER JOIN UserInfo ON UserInfo.UserId = Account.UserId
 				 INNER JOIN UserStatus ON UserStatus.SocialStatusId = UserInfo.SocialStatusId
-	WHERE UserInfo.SocialStatusId = 1
+	WHERE UserStatus.SocialStatus = @SocialStatus
 END
 GO
-EXEC AddTenDollars 1, 30
+EXEC AddTenDollars 'Disable', 10
+
+DROP PROCEDURE AddTenDollars
 
 ------------------------------------------------Query-6----------------------------------------------
 ----Displays a list of available funds for each client to transfer funds to any card
 ----Available transfer amount for each account on an accrual basis
 GO
-SELECT Account.AccountName, Account.AccountBalance, CardInfo.CardBalance, SUM(Account.AccountBalance - CardInfo.CardBalance) OVER(PARTITION BY Account.AccountName ORDER BY CardInfo.CardBalance DESC) AS AvailableTransferAmount
+SELECT Account.AccountName, SUM(Account.AccountBalance - CardInfo.CardBalance) OVER(PARTITION BY Account.AccountId ORDER BY CardInfo.CardBalance) AS AvailableTransferAmount
 FROM Account INNER JOIN CardInfo ON CardInfo.AccountId = Account.AccountId
 
 ------------------------------------------------Query-7----------------------------------------------
@@ -381,6 +381,8 @@ END
 --------------------------------------------------Tests for trigger---------------------------------------------
 DROP TRIGGER CardInfoTrigger
 DROP TRIGGER AccountTrigger
+
+DROP PROCEDURE TransactionProcedure
 
 --Immitation of transaction for card
 DECLARE @CardBalance decimal(6,2) = 10.25
